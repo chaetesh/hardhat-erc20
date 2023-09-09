732fa9dfe86ee9c001924141979cccd0830f32d3
chaetesh <chaetesh@gmail.com>
2023-09-09 11:28:56 +0530

    Done

---
+++ README.md
@@ -0,0 +1,13 @@
+# Creating Our Own Custom token in BlockChain using openzeppelin, contract deployed at (0x9EBac0c82e392211b5a2e8695498e1635E721edB) on goerli testnet
+
+This project creates a custom token in blockchain using OpenZeppelin. The token is a fungible token (ERC20), which means that each token is identical to the next. The token has a total supply of 50 tokens, and it can be transferred between accounts.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat help
+npx hardhat test
+REPORT_GAS=true npx hardhat test
+npx hardhat node
+npx hardhat run scripts/deploy.js
+```
---
+++ hardhat.config.js
@@ -0,0 +1,37 @@
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
+require("solidity-coverage");
+require("hardhat-deploy");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  // solidity: "0.8.19",
+  solidity: {
+    compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/lFlEqs4A-_jmelDXJeTXPhDK15PBnFL9",
+      accounts: [
+        "0x36e81a7c346508f7f67acff5338dec1c50b7d3953e0832e61f5f9b2b354d412c",
+      ],
+      blockConfirmations: 6,
+      chainId: 5,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      // accounts hardhat will place by default when running hardhat on localhost
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+    },
+  },
+  etherscan: {
+    apiKey: "Y1KDYSC31H5GJGDGRMUTEP1BT4FF1NDHGX",
+  },
+};
